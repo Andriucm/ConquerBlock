3a5a257ce9c43bd6b757eb7f30184ad8f5ab1034
Andrei Draghici <102661365+Andriucm@users.noreply.github.com>
2026-03-17 22:29:33 +0100

    Add github actions config for deploy

--- Astro/astro-project/astro.config.mjs
+++ Astro/astro-project/astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
 	i18n: {
+		site: "https://Andriucm.github.io",
+		base: "/ConquerBlock/Astro/astro-project",
 		defaultLocale: "es",
 		locales: ["es", "en"],
 		routing: {
